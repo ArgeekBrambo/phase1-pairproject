ac516f3b23e891349d5e07b0ab0cbc1c293239c3
argeekbrambo <argi.bramantya@gmail.com>
2022-12-08 18:21:44 +0700

    revisi-adminpage

--- migrations/20221110051119-create-patient.js
+++ migrations/20221110051119-create-patient.js
@@ -10,7 +10,8 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       status: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        defaultValue: 'sakit'
       },
       DoctorId: {
         type: Sequelize.INTEGER,
